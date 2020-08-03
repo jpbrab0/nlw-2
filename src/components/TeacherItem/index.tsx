20867122273d89758ecb77500076a834072846f0
João Pedro <jpresende502@gmail.com>
2020-08-03 19:35:48 -0300

    Adicionando arquivos

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import '../../Pages/TeacherList/styles.css'
+
+interface TeacherItemProps {
+    img?:string;
+  name: string;
+  company: string;
+  description: string;
+  price: string;
+}
+const TeacherItem: React.FC<TeacherItemProps> = (props) => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars1.githubusercontent.com/u/58999202?s=460&u=450f987552a42e720e85d49e9226122f824cf1bb&v=4"
+          alt="jpbrab0"
+        />
+        <div>
+          <strong>{props.name}</strong>
+          <span>{props.company}</span>
+        </div>
+      </header>
+        <p>{props.description}</p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {props.price}</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+export default TeacherItem
